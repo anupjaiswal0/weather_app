8fda92d3ccb99a7f4c7df9730fc9845af6684a83
Siddhi <131698330+siddibavalekar@users.noreply.github.com>
2024-02-15 14:26:59 +0530

    i have added background images and fetchcity

--- script.js
+++ script.js
@@ -2,20 +2,40 @@ async function fetchWeatherData(cityName){
 	let url =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a497f7ff2974e855df3dee5fb72c5f95`;
 	const response=await fetch(url)
 	const data=await response.json();
-	console.log(data)
+	let city = document.getElementById("city");
+	city.innerHTML= data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML=data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+" g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
+	
 }
 // demo()
 
-function fetchCity()
-{
-	let cityName=document.getElementById("city_name");
-	if(cityName.value===""){
-		alert("Enter a city name");
-	}else
-	{
-		fetchWeatherData(cityName.value);
-		cityName.value="";
-	}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
 
 
